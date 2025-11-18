250a636e384d55509a6c3ed0a64762e1c812e894
Thomas Ryan <thomas.ryan@kneat.com>
2025-11-18 22:45:20 +0000

    move header, footer and nav content to .js file add images to nav

---
+++ script.js
@@ -0,0 +1,44 @@
+
+document.getElementById("header").innerHTML = `
+<header>
+    <a class="logo" href="index.html">
+        <img src="images/home-logo.jpeg" alt="Site Logo">
+    </a>
+</header>
+`;
+
+// NAVIGATION
+document.getElementById("nav").innerHTML = `
+<nav>
+  <ul>
+    <li><a href="index.html">
+      <img class="icon" src="images/home-nav.jpeg" alt="Home">
+      <span class="text">Home</span>
+    </a></li>
+    <li><a href="services.html">
+      <img class="icon" src="images/services-nav.jpeg" alt="Services">
+      <span class="text">Services</span>
+    </a></li>
+    <li><a href="book.html">
+      <img class="icon" src="images/book-nav.jpeg" alt="Book">
+      <span class="text">Book</span>
+    </a></li>
+    <li><a href="about.html">
+      <img class="icon" src="images/about-nav.jpeg" alt="About">
+      <span class="text">About</span>
+    </a></li>
+    <li><a href="contact.html">
+      <img class="icon" src="images/contact-nav.jpeg" alt="Contact">
+      <span class="text">Contact</span>
+    </a></li>
+  </ul>
+</nav>
+`;
+
+
+// FOOTER
+document.getElementById("footer").innerHTML = `
+<footer>
+    <p>&copy; 2025 FRS Farm Relief Services</p>
+</footer>
+`;
