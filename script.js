0e8eea9dc0baadf06d8baec6a9234261dc208692
Thomas Ryan <thomas.ryan@kneat.com>
2025-11-19 19:10:12 +0000

    update nav images

--- script.js
+++ script.js
@@ -12,23 +12,23 @@ document.getElementById("nav").innerHTML = `
 <nav>
   <ul>
     <li><a href="index.html">
-      <img class="icon" src="images/home-nav.jpeg" alt="Home">
+      <img class="icon" src="images/home-icon.svg" alt="Home">
       <span class="text">Home</span>
     </a></li>
     <li><a href="services.html">
-      <img class="icon" src="images/services-nav.jpeg" alt="Services">
+      <img class="icon" src="images/services-icon.svg" alt="Services">
       <span class="text">Services</span>
     </a></li>
     <li><a href="book.html">
-      <img class="icon" src="images/book-nav.jpeg" alt="Book">
+      <img class="icon" src="images/book-icon.svg" alt="Book">
       <span class="text">Book</span>
     </a></li>
     <li><a href="about.html">
-      <img class="icon" src="images/about-nav.jpeg" alt="About">
+      <img class="icon" src="images/about-icon.svg" alt="About">
       <span class="text">About</span>
     </a></li>
     <li><a href="contact.html">
-      <img class="icon" src="images/contact-nav.jpeg" alt="Contact">
+      <img class="icon" src="images/contact-icon.svg" alt="Contact">
       <span class="text">Contact</span>
     </a></li>
   </ul>
